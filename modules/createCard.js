a5a49552b89cbd37d1ab08c6d9403b321937a6a7
Begzod <dusmamatov46@gmail.com>
2023-01-10 17:52:26 +0500

    fixed some things

--- modules/createCard.js
+++ modules/createCard.js
@@ -1,5 +1,39 @@
 let addForm = document.forms.addcard
+let select = document.querySelector('#currency')
 let {id} = JSON.parse(localStorage.getItem('user')) 
+let symbols = JSON.parse(localStorage.getItem('symbols')) 
+
+const getAllcurrency = () => {
+    fetch('https://api.apilayer.com/fixer/symbols', {
+        method: 'GET',
+        redirect: 'follow',
+        headers: {
+            "apikey": "WGNpX0DW8LKZaGewCmUJl0Uex7J9z82x",
+            "Content-Type": "application/json"
+        }
+    })
+    .then(res => res.json())
+    .then(res => {
+        localStorage.setItem('symbols', JSON.stringify(res.symbols))        
+        setSymbols(res.symbols)
+    })
+}
+
+const setSymbols = (data) => {
+    // select
+    for(let key in data) {
+        let opt = new Option(key, key)
+
+        select.append(opt)
+    }
+
+}
+
+if(!symbols) {
+    getAllcurrency()
+} else {
+    setSymbols(symbols)
+}
 
 addForm.onsubmit = (e) => {
     e.preventDefault()
