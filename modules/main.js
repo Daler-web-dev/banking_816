15d97c744872d9687e0cfab4deac9ba5341feeab
Asadbek Xamrakulov <asadhamrakulov4@gmail.com>
2023-01-10 16:39:41 +0500

    asad

---
+++ modules/main.js
@@ -0,0 +1,9 @@
+// let localed = JSON.parse(localStorage.getItem('user'))
+
+// let h1 = document.querySelector('h1')
+// let email = document.querySelector('.email')
+
+// h1.innerHTML = ''
+// email.innerHTML = ''
+// h1.innerHTML = 'Добро Пожаловать' + localed.name + localed.surname
+// email.innerHTML = localed.email
